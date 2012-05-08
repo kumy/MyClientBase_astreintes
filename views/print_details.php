bf53bc13dc85eed7b5fa171c87ce12ae0ba0a3bf
Mathieu Alorent <github@kumy.net>
2012-05-08 23:32:36 +0200

    first attempt to make a timeline

--- views/print_details.php
+++ views/print_details.php
@@ -72,7 +72,48 @@ To: <?php echo strftime('%d-%m-%y', $astreinte->end_date); ?><br />
         ?>
     </tr>
 </table>
+<br />
+<?php
 
+$width = 1500;
+$timeline_width = $astreinte->end_date - $astreinte->start_date +86400;
+$time_unit = $timeline_width / $width;
+
+
+function getTimes ($astreinte, $start, $duration, $width, $timeline_width, $time_unit) {
+
+    $hours = preg_split('/:/', $duration);
+    $ticket_start_time = new DateTime(); $ticket_start_time->setTimestamp($start);
+    $ticket_end_time   = clone $ticket_start_time; $ticket_end_time->add(new DateInterval("PT$hours[0]H$hours[1]M"));
+
+    $ticket_start = round (($start - $astreinte->start_date) / $time_unit);
+    $ticket_width = round (($ticket_end_time->getTimestamp() - $start) / $time_unit);
+
+    return array( 'start' => $ticket_start, 'end' => $ticket_width);
+}
+
+echo "<div style='width: ".(9+$width)."px; height: 22px; border: 0px solid red; position: relative;'>";
+
+$nbday = ($astreinte->end_date - $astreinte->start_date +86400) / 86400;
+for ( $i = 1; $i <= $nbday; $i++) {
+  echo "<div style='float: left; width: ".round(6* 3600 /$time_unit)."px; height: 22px; background-color: blue'></div>";
+  echo "<div style='float: left; width: ".round(16*3600 /$time_unit)."px; height: 22px; background-color: orange'></div>";
+  echo "<div style='float: left; width: ".round(2* 3600 /$time_unit)."px; height: 22px; background-color: cyan'></div>";
+}
+
+foreach ($astreinte_intervention as $inter) {
+
+    $tktimebilled = getTimes ($astreinte, $inter->start_date_time_billed, $inter->duration_billed, $width, $timeline_width, $time_unit);
+    echo "<div style='position: absolute; background-color: green; top: 3px; height: 17px; border: 0px solid green; left: ". $tktimebilled['start'] ."px; width: ". $tktimebilled['end'] ."px;'>";
+    $tktime = getTimes ($astreinte, $inter->start_date_time, $inter->duration, $width, $timeline_width, $time_unit);
+    echo "<div title='$inter->ticket_id' style='position: relative; background-color: yellow; top: 2; height: 13px; border: 0px solid yellow; left: ". ($tktime['start'] - $tktimebilled['start']) ."px; width: ". $tktime['end'] ."px;'>";
+    echo "<div style='position: absolute; background-color: red; top: 1; height: 11px; left: 1px; width: ". ($tktime['end']-2) ."px;'>";
+    echo "</div>";
+    echo "</div>";
+    echo "</div>";
+}
+echo "</div>";
+?>
 <?php } else {?>
 	<p><?php echo $this->lang->line('no_records_found');?>.</p><br />
 <?php }?>
